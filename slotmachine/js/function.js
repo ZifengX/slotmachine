c15f533acde64119e7a63083e329b9a3109add46
Zifeng Xu <xuzifeng89@gmail.com>
2014-10-27 13:41:11 -0400

    rename files, add spin button

---
+++ slotmachine/js/function.js
@@ -0,0 +1,22 @@
+﻿window.onload = function () {
+    var theCanvas = document.getElementById('myCanvas');
+    var ctx = theCanvas.getContext("2d");
+
+    var slot = new Image();
+    slot.src = "images/slotmachine.png";
+    slot.onload = drawSlot;
+
+  function drawSlot() {
+      ctx.drawImage(slot, 0, 0, 380, 400);
+  }
+
+  var stage;
+  stage = new createjs.Stage("myCanvas");
+  function drawButtons() {
+      spinButton = new createjs.Bitmap("images/spingreen.png");
+      spinButton.x = 100;
+      stage.addChild(spinButton);
+      stage.update();
+
+  }
+}
